06de1fd8094a4fd0429997ecb0e895339e566c23
lyonnjs <layonrojasyw@hotmail.com>
2024-11-28 01:25:08 -0300

    feat: PSC ProjectA3

---
+++ src/main/java/com/mycompany/a3/testpsc/User.java
@@ -0,0 +1,43 @@
+package com.mycompany.a3.testpsc;
+
+public final class User
+{   private String  sUser;
+    private String  sPass;
+    private String  sEmail;
+    
+    public  User()
+    {   clearObject();
+    }
+    public  User(String sE, String sU, String sP)
+    {   sUser = sU;
+        sPass = sP;
+        sEmail = sE;
+    }
+
+    public String getEmail() {
+        return sEmail;
+    }
+
+    public void setEmail(String sEmail) {
+        this.sEmail = sEmail;
+    }
+    
+    public  String  getUser()
+    {   return  sUser;
+    }
+    public  void    setUser(String sU)
+    {   sUser = sU;
+    }
+    
+    public  String  getPass()
+    {   return  sPass;
+    }
+    public  void    setPass(String sP)
+    {   sPass = sP;
+    }
+    public  void    clearObject()
+    {   sUser = "Não há usuário cadastrado";
+        sPass = "Não há senha cadastrada";
+        sEmail = "Não há email cadastrado";
+    }
+}
